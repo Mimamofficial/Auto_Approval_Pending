5318da485153648966f0fcaa79d55526f65fff9b
Mimamofficial <mrmuzaffarimam@gmail.com>
2025-09-01 09:43:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ API_HASH = environ.get("API_HASH", "567c6df308dc6901790309499f729d12")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002154224937"))
-ADMINS = int(environ.get("ADMINS", "6139759254"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002338165303"))
+ADMINS = int(environ.get("ADMINS", "1038493510"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://mohammadmuzaffarimambaturbari:sHXNxpKZ9PDjyYQr@cluster0.dqjjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://mrnoffice692:PsO4VGHI9heKd7WA@cluster0.o1vcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
